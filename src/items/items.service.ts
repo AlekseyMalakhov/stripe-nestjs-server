39f59276fd9d6236d9eff4d7ba4c07b9b19ff9c1
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-01-09 15:42:40 +0200

    remove pg

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,16 +1,11 @@
 import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
-import { Pool } from "pg";
 
 @Injectable()
 export class ItemsService {
     constructor(private configService: ConfigService) {}
-    pool = new Pool();
 
     async findAll() {
-        const client = await this.pool.connect();
-        const res = await client.query("select * from items");
-        client.release();
-        return res.rows;
+        return "hello world";
     }
 }
