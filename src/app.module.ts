5ae404661a121daa778986fe739433433a8550ec
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-01-09 14:21:28 +0200

    some clean add pg

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
 import { ItemsModule } from "./items/items.module";
 
 @Module({
-    imports: [ItemsModule],
-    controllers: [AppController],
-    providers: [AppService],
+    imports: [ConfigModule, ItemsModule],
 })
 export class AppModule {}
