e13d98e3a1eb05001e3bb63684ae83499ce6d2c6
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-01-10 14:18:00 +0200

    add stripe

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -3,7 +3,7 @@ import { PaymentService } from "./payment.service";
 import { CreatePaymentDto } from "./dto/create-payment.dto";
 import { UpdatePaymentDto } from "./dto/update-payment.dto";
 
-@Controller("payment")
+@Controller("payment-intent")
 export class PaymentController {
     constructor(private readonly paymentService: PaymentService) {}
 
@@ -12,23 +12,23 @@ export class PaymentController {
         return this.paymentService.create(createPaymentDto);
     }
 
-    @Get()
-    findAll() {
-        return this.paymentService.findAll();
-    }
+    // @Get()
+    // findAll() {
+    //     return this.paymentService.findAll();
+    // }
 
-    @Get(":id")
-    findOne(@Param("id") id: string) {
-        return this.paymentService.findOne(+id);
-    }
+    // @Get(":id")
+    // findOne(@Param("id") id: string) {
+    //     return this.paymentService.findOne(+id);
+    // }
 
-    @Patch(":id")
-    update(@Param("id") id: string, @Body() updatePaymentDto: UpdatePaymentDto) {
-        return this.paymentService.update(+id, updatePaymentDto);
-    }
+    // @Patch(":id")
+    // update(@Param("id") id: string, @Body() updatePaymentDto: UpdatePaymentDto) {
+    //     return this.paymentService.update(+id, updatePaymentDto);
+    // }
 
-    @Delete(":id")
-    remove(@Param("id") id: string) {
-        return this.paymentService.remove(+id);
-    }
+    // @Delete(":id")
+    // remove(@Param("id") id: string) {
+    //     return this.paymentService.remove(+id);
+    // }
 }
--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -1,26 +1,38 @@
 import { Injectable } from "@nestjs/common";
 import { CreatePaymentDto } from "./dto/create-payment.dto";
-import { UpdatePaymentDto } from "./dto/update-payment.dto";
+import Stripe from "stripe";
+const stripe = new Stripe(process.env.STRIPE_KEY);
 
 @Injectable()
 export class PaymentService {
-    create(createPaymentDto: CreatePaymentDto) {
-        return "This action adds a new payment";
+    async create(createPaymentDto: CreatePaymentDto) {
+        const price = 10;
+
+        // Create a PaymentIntent with the order amount and currency
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: price,
+            currency: "usd",
+            automatic_payment_methods: {
+                enabled: true,
+            },
+        });
+
+        return { clientSecret: paymentIntent.client_secret };
     }
 
-    findAll() {
-        return `This action returns all payment`;
-    }
+    // findAll() {
+    //     return `This action returns all payment`;
+    // }
 
-    findOne(id: number) {
-        return `This action returns a #${id} payment`;
-    }
+    // findOne(id: number) {
+    //     return `This action returns a #${id} payment`;
+    // }
 
-    update(id: number, updatePaymentDto: UpdatePaymentDto) {
-        return `This action updates a #${id} payment`;
-    }
+    // update(id: number, updatePaymentDto: UpdatePaymentDto) {
+    //     return `This action updates a #${id} payment`;
+    // }
 
-    remove(id: number) {
-        return `This action removes a #${id} payment`;
-    }
+    // remove(id: number) {
+    //     return `This action removes a #${id} payment`;
+    // }
 }
